233dcec2cc6402b6c7aae13f945b8ecee6e053d1
juanh <juanh@LAPTOP-42K13HIQ>
2018-11-02 21:45:36 -0600

    insercion en arbol binario busqueda

--- DataStructures/src/dataStructures/BinaryTree.java
+++ DataStructures/src/dataStructures/BinaryTree.java
@@ -8,7 +8,45 @@ package dataStructures;
 /**
  *
  * @author juanh
+ * 
+ * arbol binario de busqueda donde para todo nodo en el se cumple que 
+ * los valores almacenados en su subarbol izq son menores
+ * y los almacenados en el subarbol der son mayores
+ * este arbol no admitira valores repetidos y trabajara con enteros
+ * sus atributos seran publicos
  */
 public class BinaryTree {
+    public NodeB raiz;
+    
+    public BinaryTree (){
+        raiz = null;
+    }
+    
+    public BinaryTree (int dato) {
+        raiz = new NodeB (dato);
+    }
+    
+    public void insert (int dato) {
+        if (raiz == null) 
+            raiz = new NodeB (dato);
+        else
+            insert (raiz, dato);
+    }
+    
+    private NodeB insert(NodeB nodo, int dato) {
+        if (nodo == null) {
+            nodo = new NodeB (dato);
+            return nodo;
+        } else if (dato < nodo.dato)
+            nodo.izq = insert (nodo.izq, dato);
+        else if (dato > nodo.dato)
+            nodo.der = insert (nodo.der, dato);
+        return nodo;
+    }
+    
+    
+    
     
 }
+
+    
